c1401fb789473c727fd2452d10dea5eef83ca5bf
Aflah <72096386+aflah02@users.noreply.github.com>
2022-09-13 12:28:16 +0530

    Changes

--- Word_Embeddings/Embeddings-via-spacy-universal-sentence-encoder.py
+++
@@ -1,8 +0,0 @@
-import spacy_universal_sentence_encoder
-# load one of the models: ['en_use_md', 'en_use_lg', 'xx_use_md', 'xx_use_lg']
-nlp = spacy_universal_sentence_encoder.load_model('en_use_lg')
-# get two documents
-doc_1 = nlp('Hi there, how are you?')
-doc_2 = nlp('Hello there, how are you doing today?')
-# use the similarity method that is based on the vectors, on Doc, Span or Token
-print(doc_1.similarity(doc_2[0:7]))
